e76a75dd117d2d735ad3deac3e9b1196a621038d
KAMRUZZAMAN <zaman.eyz@gmail.com>
2021-01-13 12:06:37 +0600

    add api auth by sanctum

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only(['user','logout']);
+    }
+
+    public function user(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        return response()->json(['user' => $user],200);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function login(Request $request) : JsonResponse
+    {
+        $this->validate($request,[
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+
+            $token = $user->createToken($request->email);
+
+            return response()->json(['token' => $token->plainTextToken], 201);
+        }
+        else{
+
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        
+        
+
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $this->validate($request,[
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'unique:users'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if ($user) {
+            //create token
+            $token = $user->createToken($request->email);
+
+            return response()->json(['token' => $token->plainTextToken], 201);
+        }
+        else{
+            //error message
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+    }
+
+    protected function credentials($request)
+    {
+        return $request->only('email', 'password');
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->tokens->each(function($token, $key) {
+            $token->delete();
+        });
+        return response()->json(['message' =>'You are Successfully Logged out'], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/',function(){
+    return response()->json([
+        'success' => true,
+        'message' => 'api under construction'
+    ], 200);
+});
+
+
+Route::prefix('v1')->group(function () {
+    //get current user
+    Route::get('user', [AuthController::class,'user']);
+
+    //Auth routes
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('register', [AuthController::class,'register']);
+    //logout
+    Route::post('logout', [AuthController::class,'logout']);
 });
